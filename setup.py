c45162f93c3902cf65c34dc5de44d7d2da9c4d3b
animeshjainaroo5 <animeshjain.chd@gmail.com>
2025-07-03 13:56:37 +0530

    Setup and requirement

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_e_dot = '-e .'
+
+def get_req(file_path:str)->List[str]:
+    #returns the list of requirements 
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+
+setup(
+    name="ML Project",
+    version="0.0.1",
+    author="Animesh Jain",
+    author_email="animeshjain.chd@gmail.com",
+    packages = find_packages(),
+    install_requirements = get_req('requirements.txt')
+)
